7f2d829ece5e882c2d248875f734456909c15159
Keith <keithgdowney@gmail.com>
2018-10-27 16:02:22 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,174 @@
+// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+
+// ```json
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+// ```
+[
+    {
+    "name":"Mark Lizardberg",
+    "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwierPW7v6DeAhVoyVQKHeIwBMQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DEw9oewvyd0M&psig=AOvVaw03o_7WUJbhYIH3Vzfe8L7n&ust=1540519213350011",
+    "scores": [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10
+    ]},
+
+    {
+        "name":"Cast of Friends",
+        "photo":"https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/22/13/why-monicas-apartment-on-friends-was-purple.jpg?w968h681",
+        "scores": [
+            10,
+            9,
+            8,
+            7,
+            6,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]},
+
+        {
+            "name":"Tim Allen",
+            "photo":"https://i.ytimg.com/vi/a6g0pOZus8o/maxresdefault.jpg",
+            "scores": [
+                2,
+                5,
+                7,
+                8,
+                4,
+                1,
+                10,
+                9,
+                6,
+                3
+            ]},
+            {
+                "name":"Alex Jones",
+                "photo":"https://pixel.nymag.com/imgs/daily/selectall/2017/05/03/03-alex-jones-supplements.w700.h700.jpg",
+                "scores": [
+                    5,
+                    6,
+                    7,
+                    8,
+                    9,
+                    4,
+                    3,
+                    1,
+                    10,
+                    2
+                ]},
+                {
+                    "name":"Pewdiepie",
+                    "photo":"https://vignette.wikia.nocookie.net/smosh/images/c/c8/0f30bac74e77641bc662e73b3b4990d3.jpeg/revision/latest?cb=20180712122240",
+                    "scores": [
+                        4,
+                        5,
+                        6,
+                        7,
+                        8,
+                        9,
+                        10,
+                        1,
+                        2,
+                        3
+                    ]},
+                    {
+                        "name":"Doctor Who",
+                        "photo":"https://cdn.vox-cdn.com/thumbor/Tbdxkr2SWlT3XSA-ZrKyINXqLrg=/0x0:618x362/1200x800/filters:focal(260x132:358x230)/cdn.vox-cdn.com/uploads/chorus_image/image/59829073/doctor_who_50_years.0.jpeg",
+                        "scores": [
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9,
+                            10,
+                            1,
+                            2
+                        ]},
+                        {
+                            "name":"Woman",
+                            "photo":"https://cdn.images.express.co.uk/img/dynamic/galleries/x701/389530.jpg",
+                            "scores": [
+                                6,
+                                7,
+                                8,
+                                9,
+                                10,
+                                1,
+                                2,
+                                3,
+                                4,
+                                5
+                            ]},
+                            {
+                                "name":"Man",
+                                "photo":"https://ichef.bbci.co.uk/images/ic/640x360/p01t1fqt.jpg",
+                                "scores": [
+                                    7,
+                                    8,
+                                    9,
+                                    10,
+                                    1,
+                                    2,
+                                    3,
+                                    4,
+                                    5,
+                                    6
+                                ]},
+                                {
+                                    "name":"Joe",
+                                    "photo":"https://thenypost.files.wordpress.com/2017/08/shutterstock_157245107.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+                                    "scores": [
+                                        8,
+                                        9,
+                                        10,
+                                        1,
+                                        2,
+                                        3,
+                                        4,
+                                        5,
+                                        6,
+                                        7
+                                    ]},
+                                    {
+                                        "name":"Her",
+                                        "photo":"https://news.artnet.com/app/news-upload/2018/08/Laura-Hoptmon_Preview02_MSeck_300res-1500x826.jpg",
+                                        "scores": [
+                                            9,
+                                            10,
+                                            1,
+                                            2,
+                                            3,
+                                            4,
+                                            5,
+                                            6,
+                                            7,
+                                            8
+                                        ]},
+]
